774bb19f087f639497dd17c54bd0607bdd610c6b
rouanis5 <ouanis42@gmail.com>
2022-06-28 17:28:04 +0100

    init
    add init files, install vite, sass, autoprefixer, vite/legacy

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import legacy from "@vitejs/plugin-legacy";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: resolve(__dirname , 'src'),
+  build: {
+    outDir: resolve(__dirname , 'dist'),
+    emptyOutDir: true,
+  },
+  plugins: [
+    legacy(),
+  ],
+});
