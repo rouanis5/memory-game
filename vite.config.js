78774b3fb84c52294979bbec849ead62df6135a9
rouanis5 <ouanis42@gmail.com>
2022-06-28 21:24:00 +0100

    correct prettier scripts
    apply changes

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
-import legacy from "@vitejs/plugin-legacy";
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import legacy from '@vitejs/plugin-legacy'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: resolve(__dirname , 'src'),
+  root: resolve(__dirname, 'src'),
   build: {
-    outDir: resolve(__dirname , 'dist'),
+    outDir: resolve(__dirname, 'dist'),
     emptyOutDir: true,
   },
-  plugins: [
-    legacy(),
-  ],
-});
+  plugins: [legacy()],
+})
