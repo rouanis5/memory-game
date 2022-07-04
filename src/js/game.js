c23e3877334e72c102c770a826894dcd45cea883
rouanis5 <ouanis42@gmail.com>
2022-07-04 21:49:21 +0100

    +sidebar, btns, js improvment
    +stopGame(), +reset()

--- src/js/game.js
+++ src/js/game.js
@@ -1,45 +1,55 @@
 import boardView from './views/board'
 import { play } from './helpers/audio'
 import { setAdvancedInterval } from './helpers/functions'
-import { state, params, audio } from './config'
+import { state as stateOriginal, params, audio } from './config'
 import getIcons from './helpers/icons'
 
+let state = { ...stateOriginal }
 let interval = null
 const { cardIdAttr: idAttr } = params // id attaribute
 
 const selectors = {
-  moves: document.getElementById('moves'),
   board: document.getElementById('board'),
+  welcom: document.getElementById('welcom'),
   game: document.getElementById('game'),
-  time: document.getElementById('time'),
-  start: document.getElementById('start'),
-  matched: document.getElementById('matched'),
-  remained: document.getElementById('remained'),
+
+  sidebar: {
+    start: document.getElementById('start'),
+    stop: document.getElementById('stop'),
+    restart: document.getElementById('restart'),
+  },
+
+  scores: {
+    moves: document.getElementById('moves'),
+    matched: document.getElementById('matched'),
+    remained: document.getElementById('remained'),
+    time: document.getElementById('time'),
+  },
+}
+
+function stopGame() {
+  // set of actions to run
+  params.timer.stop() // stop the timer
+  clearInterval(interval) // stop updating time on the screen
+  audio.running.pause() // stop running audio
+  selectors.game.classList.add('freeze') // stop all events on the game board
 }
 
 // check if the game is finished
 function isGameOver() {
-  function actions() {
-    // set of actions to run
-    params.timer.stop() // stop the timer
-    clearInterval(interval) // stop updating time on the screen
-    audio.running.pause() // stop running audio
-    selectors.game.classList.add('freeze') // stop all events on the game board
-
-    state.time = params.timer.getMilliseconds() // save the game time in state.time
-  }
-
   // if you win the game
   if (state.matched === params.icons.length) {
+    stopGame()
     state.win = true
-    actions()
+    state.time = params.timer.getMilliseconds() // save the game time in state.time
     return
   }
 
   // if you lose the game
   if (state.wrong === params.maxTries) {
+    stopGame()
     play(audio.over, { delay: params.delay })
-    actions()
+    state.time = params.timer.getMilliseconds() // save the game time in state.time
   }
 }
 
@@ -52,7 +62,7 @@ function analyse(cards) {
   // check if there is two flipped card
   if (flipped.length === 2) {
     state.moves += 1 // increase moves
-    selectors.moves.textContent = state.moves // display moves in the screen
+    selectors.scores.moves.textContent = state.moves // display moves in the screen
 
     // unflip cards and wait a delay
     setTimeout(() => {
@@ -65,7 +75,7 @@ function analyse(cards) {
       flipped.map((el) => el.classList.add('matched')) // add match class (display cards & remove event listner)
       play(audio.reward, soundDelay) // play the reward sound
       state.matched += 1 // increase matched cards
-      selectors.matched.textContent = state.matched // display it
+      selectors.scores.matched.textContent = state.matched // display it
 
       isGameOver() // check if the game finished
       return
@@ -73,7 +83,7 @@ function analyse(cards) {
 
     // play sanction sound and inscrease wrong tries
     state.wrong += 1
-    selectors.remained.textContent = params.maxTries - state.wrong // display remaind tries
+    selectors.scores.remained.textContent = params.maxTries - state.wrong // display remaind tries
     play(audio.sanction, soundDelay)
     isGameOver() // check if the game finished
   }
@@ -86,7 +96,14 @@ function run() {
   selectors.game.classList.remove('d-none')
   selectors.game.innerHTML = boardView(icons).html // fill board with cards
 
-  selectors.remained.textContent = params.maxTries - state.wrong // display remaind tries
+  // freeze start btn
+  selectors.sidebar.start.classList.add('freeze')
+  // remove freezing events
+  selectors.game.classList.remove('freeze')
+  selectors.sidebar.stop.classList.remove('freeze')
+  selectors.sidebar.restart.classList.remove('freeze')
+
+  selectors.scores.remained.textContent = params.maxTries - state.wrong // display remaind tries
 
   play(audio.running, { loop: true }) // start infinite running sound
   params.timer.start() // start the timer
@@ -104,8 +121,40 @@ function run() {
 
   // update the timer every  1000ms
   interval = setAdvancedInterval(() => {
-    selectors.time.textContent = Math.trunc(params.timer.getSeconds())
+    selectors.scores.time.textContent = Math.trunc(params.timer.getSeconds())
   }, 1000)
 }
 
-selectors.start.addEventListener('click', run)
+function resetScreen() {
+  selectors.sidebar.start.classList.remove('freeze') // unfreeze start btn
+  selectors.sidebar.stop.classList.add('freeze') // freeze stop btn
+  selectors.sidebar.restart.classList.add('freeze') // freeze retart btn
+
+  // hide all board childrens
+  Array.from(selectors.board.children).map((el) => el.classList.add('d-none'))
+
+  // reset all numbers
+  Object.entries(selectors.scores).forEach((el) => {
+    // eslint-disable-next-line no-param-reassign
+    el[1].textContent = '0'
+  })
+
+  state = { ...stateOriginal } // reset sate
+  selectors.game.innerHTML = '' // reset game
+}
+
+function stop() {
+  stopGame()
+  resetScreen()
+  selectors.welcom.classList.remove('d-none') // display welecom section
+}
+
+function restart() {
+  stopGame()
+  resetScreen()
+  run()
+}
+
+selectors.sidebar.start.addEventListener('click', run)
+selectors.sidebar.stop.addEventListener('click', stop)
+selectors.sidebar.restart.addEventListener('click', restart)
