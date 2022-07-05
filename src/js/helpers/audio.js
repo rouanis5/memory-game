3b3885a449fd2c77c02a962a0e61f1f741bd78e7
rouanis5 <ouanis42@gmail.com>
2022-07-06 00:39:33 +0100

    + stop(), stopAudios()
    + verify audio param if is instance of Audio

--- src/js/helpers/audio.js
+++ src/js/helpers/audio.js
@@ -1,6 +1,8 @@
 // it returns an object after converting the value (url) to an audio object
 function setAudios(audioObject) {
   const audios = audioObject
+  if (!audios) return {} // stop if there are no audios
+
   Object.entries(audios).forEach(([title, url]) => {
     // audios[title] = new Audio(new URL(`../${url}`, import.meta.url).href)
     audios[title] = new Audio(url)
@@ -8,9 +10,28 @@ function setAudios(audioObject) {
   return audios
 }
 
+// stop audios
+function stop(sound) {
+  const audio = sound
+  if (!(audio instanceof Audio)) return // stop if there is no audio
+  audio.pause()
+}
+
+function stopAudios(audioObject) {
+  const audios = audioObject
+  if (!audios) return {} // stop if there are no audios
+
+  Object.entries(audios).forEach(([title]) => {
+    stop(audios[title])
+  })
+  return {}
+}
+
 // play the sound correctly with parameters of delay and loop
 function play(sound, { delay = 0, loop = false } = {}) {
   const audio = sound
+  if (!(audio instanceof Audio)) return // stop if there is no audio
+
   function runSound() {
     // take sure to start the sound from the begining
     audio.pause()
@@ -32,4 +53,4 @@ function play(sound, { delay = 0, loop = false } = {}) {
   setTimeout(runSound, delay)
 }
 
-export { play, setAudios }
+export { play, setAudios, stopAudios, stop }
